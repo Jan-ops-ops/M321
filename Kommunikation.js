290dd848a8dbe938aca03993f78f0d37d608380c
Wisniewski Wiktor <wiktor_wisniewski@sluz.ch>
2026-08-27 13:35:19 +0200

    kommunikation v.2

--- Kommunikation.js
+++ Kommunikation.js
@@ -11,29 +11,34 @@ async function start() {
         body: JSON.stringify({ target: {x: -70565, y: 72811} }) // -70565/72811 : 4446/4340
     });
 
-    const Stationws = new WebSocket("ws://192.168.103.0:2026/api");
-    const Teamws = new WebSocket("ws://192.168.103.0:2026/api");
+    const Stationws = new WebSocket(`ws://${process.env.API_IP}:2026/api`);
+    const Teamws = new WebSocket(`ws://${process.env.API_IP}:5000/api`);
+
+    Stationws.onopen = () => {
+        console.log("Verbunden mit Stationws (Port 2026)");
+    };
+    Teamws.onopen = () => {
+        console.log("Verbunden mit Teamws (Port 5000)");
+    };
 
     Stationws.onmessage = (event) => {
-        Teamws.onopen = () => {
-    console.log("connected");
-    {
-        const data = { source: "Elyse Terminal", msg: event };
-        console.log("client -> server: " + JSON.stringify(data));
-        Teamws.send(JSON.stringify(data));
-    }
-    };
+        console.log("Stationws empfangen: " + event.data);
+
+        if (Teamws.readyState === WebSocket.OPEN) {
+            const data = { source: "Elyse Terminal", msg: event.data }; 
+            console.log("Leite weiter an Teamws: " + JSON.stringify(data));
+            Teamws.send(JSON.stringify(data));
+        }
     };
 
     Teamws.onmessage = (event) => {
-        Stationws.onopen = () => {
-    console.log("connected");
-    {
-        const data = { source: "Jan Ship", msg: event };
-        console.log("client -> server: " + JSON.stringify(data));
-        Stationws.send(JSON.stringify(data));
-    }
-    };
+        console.log("Teamws empfangen: " + event.data);
+
+        if (Stationws.readyState === WebSocket.OPEN) {
+            const data = { source: "Jan Ship", msg: event.data }; 
+            console.log("Leite weiter an Stationws: " + JSON.stringify(data));
+            Stationws.send(JSON.stringify(data));
+        }
     };
 
 
